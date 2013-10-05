af162b1b4b1b2c9d2844d1035070f488c1c27a3c
Ben Briggs <therealbenbriggs@hotmail.com>
2013-10-05 17:11:12 +0100

    Less is more.

--- tasks/available_tasks.js
+++ tasks/available_tasks.js
@@ -15,6 +15,7 @@ function formatOutput(opts) {
 module.exports = function(grunt) {
     grunt.registerTask('available_tasks', 'List available Grunt tasks & targets.', function() {
         var _       = grunt.util._,
+            _s      = _.str,
             tasks   = grunt.task._tasks;
 
         var longest = _.max(tasks, function(task) {
@@ -22,18 +23,12 @@ module.exports = function(grunt) {
         });
 
         _.each(_.sortBy(tasks, ['name']), function(task) {
-            var type,
-                colour,
-                name = task.name,
+            var name = task.name,
                 multi = (task.multi) ? '->' : '>',
                 config = grunt.config(name),
                 targets = '';
             // test if the task is a local config or something installed
-            if (_.str.include(task.meta.info, 'local Npm module')) {
-                type = multi;
-            } else {
-                type = '=>';
-            }
+            var type = (_s.include(task.meta.info, 'local Npm module')) ? multi : '=>';
             if (typeof config === 'object' && task.multi) {
                 delete config.options;
                 var conf = Object.keys(config);
@@ -43,9 +38,8 @@ module.exports = function(grunt) {
                 }
             }
             // Grey out available_tasks itself.
-            colour = (name !== 'available_tasks') ? true : false;
             grunt.log.writeln(formatOutput({
-                colour :  colour,
+                colour :  ! _s.include(name, 'available_tasks'),
                 name :    _.rpad(name, longest.name.length),
                 type :    _.center(type, 4),
                 info :    task.info,
