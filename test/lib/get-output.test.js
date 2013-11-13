a0bb0c9841e557e9d7689fff4ee3424ea56181f3
Nick Heiner <nick.heiner@opower.com>
2013-11-13 18:56:24 -0500

    test passes!

--- test/lib/get-output.test.js
+++ test/lib/get-output.test.js
@@ -1,9 +1,19 @@
 'use strict';
 
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+    getOutput = require('../../lib/get-output');
 
 describe('getOutput', function() {
 
-    it('')
+    it('handles the case where there are no groups', function() {
+        var output = [],
+            formatted = 'formatted default task name';
+        getOutput(output, {}, 'default', formatted);
+
+        expect(output).to.have.length(1);
+        expect(output[0]).to.eql({
+            log: formattedg
+        });
+    });
 
 });
