5294d3bdf4462953081ee8a3e9df9f67ffbcb26e
Ben Briggs <therealbenbriggs@hotmail.com>
2014-01-05 23:41:03 +0000

    Refactored default reporter into separate file. Custom reporter capability.

--- test/lib/get_output.test.js
+++ test/lib/get_output.test.js
@@ -5,60 +5,29 @@ var expect = require('chai').expect,
 
 describe('getOutput', function () {
 
-    it('handles the case when there are no groups', function () {
-        var output = [],
-            formatted = 'formatted default task name';
-        getOutput(output, {}, 'default', formatted);
-
-        expect(output).to.have.length(1);
-        expect(output[0]).to.eql({
-            log : formatted
-        });
+    beforeEach(function() {
+        this.output = [];
+        this.task   = {
+            name : 'mocha'
+        };
     });
 
-    it('adds multiple tasks when there are no groups', function () {
-        var output = [],
-            formatted1 = 'formatted default task name 1',
-            formatted2 = 'formatted default task name 2';
-
-        getOutput(output, {}, 'default', formatted1);
-        getOutput(output, {}, 'default', formatted2);
-
-        expect(output).to.have.length(2);
-        expect(output).to.eql([
-            {
-                log : formatted1
-            },
-            {
-                log : formatted2
-            }
-        ]);
+    it('handles the case when there are no groups', function () {
+        getOutput(this.output, {}, this.task);
+        expect(this.output).to.have.length(1);
+        expect(this.output[0].name).to.eql('mocha');
     });
 
     it('handles the case where there are groups', function () {
-        var output = [],
-            groupedTaskName = 'grouped-task',
-            ungroupedTaskName = 'ungrouped-task',
-            formattedInGroup = 'formatted task in group',
-            formattedUngrouped = 'formatted task ungrouped',
-            groups = {
-                groupA : [groupedTaskName]
-            };
+        var groups = {
+            'Lint tools' : ['jshint', 'mocha']
+        };
+        getOutput(this.output, groups, this.task);
+        getOutput(this.output, groups, {
+            name : 'jshint'
+        });
 
-        getOutput(output, groups, groupedTaskName, formattedInGroup);
-        getOutput(output, groups, ungroupedTaskName, formattedUngrouped);
-
-        expect(output).to.have.length(2);
-        expect(output).to.eql([
-            {
-                group : 'GroupA',
-                log   : formattedInGroup
-            },
-            {
-                group : 'Ungrouped',
-                log   : formattedUngrouped
-            }
-        ]);
+        expect(this.output).to.have.length(2);
+        expect(this.output[1].group).to.eql('Lint tools');
     });
-
 });
